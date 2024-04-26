aca14ab1e4283ec53b177e06073ffe4f69b9e961
saiful797 <saifulislamsaif797@gmail.com>
2024-04-26 09:15:36 +0600

    Successfully connected the mongodb server

--- index.js
+++ index.js
@@ -9,16 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// exploreTourism
-// MkoKPkPb6oSrF7hW
-
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a3qxp45.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
